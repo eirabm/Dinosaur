a82f0d599c63e1ecfb5d081cab2fc0983a4e8670
Eira Mazon <eira.b.mazon@gmail.com>
2021-10-25 12:18:38 -0500

    Fix. Dinosaur dies everytime on collision

--- dinosaur/libs/ui/src/lib/layout/layout.tsx
+++ dinosaur/libs/ui/src/lib/layout/layout.tsx
@@ -10,29 +10,50 @@ export interface LayoutProps {}
 
 export function Layout(props: LayoutProps) {
   const [playerScore, setPlayerScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
   const [startGame, setStartGame] = useState<null | boolean>(null);
   const dinoRef = useRef<HTMLDivElement>(null);
   const cactiRef = useRef<HTMLDivElement>(null);
   const dinoPosition = dinoRef?.current?.getBoundingClientRect();
   const cactiPosition = cactiRef?.current?.getBoundingClientRect();
 
-  window.addEventListener('keydown', () => {
-    if (!startGame) {
+  window.addEventListener('keydown', (e) => {
+    if (e.code === 'Space' && !startGame) {
       setStartGame(true);
     }
   });
 
-  useEffect(() => {
-    setTimeout(() => setPlayerScore(playerScore + 100), 400);
-  });
+  const detectCollision = () => {
+    const dinoTop = parseInt(
+      window
+        .getComputedStyle(dinoRef.current as Element)
+        .getPropertyValue('top')
+    );
+    const cactusLeft = parseInt(
+      window
+        .getComputedStyle(cactiRef.current as Element)
+        .getPropertyValue('left')
+    );
 
-  // console.log('dino', dinoPosition, 'cacti', cactiPosition);
-
-  if (dinoPosition && cactiPosition) {
-    if (dinoPosition.y > 600 && cactiPosition.x <= 300) {
-      console.log('game Over');
+    if (cactusLeft < 10 && dinoTop >= 250) {
+      setStartGame(false);
+      setPlayerScore(0);
     }
-  }
+  };
+
+  useEffect(() => {
+    if (startGame) {
+      setTimeout(() => setPlayerScore(playerScore + 100), 400);
+    }
+
+    const watchCollision = setInterval(() => {
+      detectCollision();
+    }, 10);
+
+    return () => {
+      clearInterval(watchCollision);
+    };
+  }, [cactiPosition, dinoPosition, playerScore, startGame]);
 
   return (
     <div className={Styles.layout}>
